10bcb6dcb57987cb1b21fe20d4b231cabc69f5f1
naveen <naveenreddy.k07@gmail.com>
2021-12-02 18:45:36 +0530

    first commit

---
+++ Gambler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
